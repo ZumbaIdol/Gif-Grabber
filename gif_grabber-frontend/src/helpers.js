ef93f943ffaa1426296b5187d5525bbae088ca9b
ZumbaIdol <david.a.foote@gmail.com>
2020-02-18 19:15:07 -0600

    Working on 1st fetch

---
+++ gif_grabber-frontend/src/helpers.js
@@ -0,0 +1,3 @@
+const urlSearch = 'https://api.giphy.com/v1/gifs/search?api_key=7x3PQolnMLSyWjEZZbWtov3SGXRutNak&q=excited&limit=25&offset=0&rating=G&lang=en'
+
+const urlTrend = 'https://api.giphy.com/v1/gifs/trending?api_key=7x3PQolnMLSyWjEZZbWtov3SGXRutNak&limit=25&rating=G'
--- gif_grabber-frontend/src/index.js
+++ gif_grabber-frontend/src/index.js
@@ -1,5 +1,17 @@
-const BACKEND_URL = "https://localhost:3000"
-fetch(`${BACKEND_URL}/test`)
-.then(response => response.json())
-.then(parsedResponse => console.log(parsedResponse))
+import { response } from "express"
+
+// const BACKEND_URL = "https://localhost:3000"
+// fetch(`${BACKEND_URL}/test`)
+// .then(response => response.json())
+// .then(parsedResponse => console.log(parsedResponse))
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector("#gif-container")
+    const gifSearch = document.querySelector("#gifName")
+    const trendSearch = document.querySelector("#trendName")
+
+    fetch(gifSearch)
+        .then(response => response.json())
+        .then(json => container.innerHTML)
+})
 
