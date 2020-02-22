d37c26129cfce29b789fef342164a6ed2a53a42b
ZumbaIdol <david.a.foote@gmail.com>
2020-02-22 09:03:36 -0600

    Added create method in gif controller

--- gif_grabber-backend/app/controllers/gifs_controller.rb
+++ gif_grabber-backend/app/controllers/gifs_controller.rb
@@ -3,4 +3,14 @@ class GifsController < ApplicationController
         @gifs = Gif.all
         render json: @gifs
     end
+
+    def create
+        current_user = Gif.find_by(params[:gif][:user_id])
+        gif = current_user.gifs.build(gif_params)
+        if @current_user.save
+          render json: gif, status: 200
+        else
+          render json: { error: "Failed to grab gif", status: 500 }, status: 500
+        end
+    end  
 end
