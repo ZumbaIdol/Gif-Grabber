57206ef11bd28d8d1f140110a4a6b0d81c1893c6
ZumbaIdol <david.a.foote@gmail.com>
2020-02-24 20:07:30 -0600

    Added contructor to gifs manager

--- gif_grabber-frontend/src/components/gifs_manager.js
+++ gif_grabber-frontend/src/components/gifs_manager.js
@@ -1,3 +1,10 @@
 class GifsManager {
-    
+    constructor() {
+        this.gifs = []
+        this.adapter = new GifsAdapter()
+        this.userAdapter = new UsersAdapter()
+
+        this.initBindingsAndEventListeners()
+        this.fetchAndLoadGifs()
+    }
 }
