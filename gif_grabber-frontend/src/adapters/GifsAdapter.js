6cbf3bddb98fbb0fba039b05d59e8a995ddb5c64
ZumbaIdol <david.a.foote@gmail.com>
2020-03-01 22:06:26 -0600

    Made some progess, seeing some DOM changes but still issues to resolve with fetch :ambulance:

--- gif_grabber-frontend/src/adapters/GifsAdapter.js
+++ gif_grabber-frontend/src/adapters/GifsAdapter.js
@@ -3,16 +3,16 @@ class GifsAdapter {
         this.baseUrl = ('https://giphy.com/embed/xTiN0CNHgoRf1Ha7CM?api_key=7x3PQolnMLSyWjEZZbWtov3SGXRutNak&q=excited&limit=25&offset=0&rating=G&lang=en')
         }
 
-        getGifs() {
-            return fetch(this.baseUrl).then(response => response.json())
-        }
+        // getGifs() {
+        //     return fetch(this.baseUrl).then(response => response.json())
+        // } This method is causing cors error, tried combining methods below, caused a different error
 
-        createGif(value, id) {
+        getAndCreateGif(value, id) {
             const gif = {
                 name: value,
                 user_id: id
             }
-            return fetch(this.baseUrl, {
+            return fetch(this.baseUrl, { // causing a 403 forbidden error
                 method: 'POST',
                 mode: 'no-cors',
                 headers: {
